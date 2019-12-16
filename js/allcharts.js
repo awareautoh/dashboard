950c95cd8772fa7145be383929e51f7fc55b2eb3
Douangtavanh <dk43454@gmail.com>
2019-12-16 17:00:29 +0700

    Add colour legend to map
    +Add function to show legend to map add the bottom of JS file

--- js/allcharts.js
+++ js/allcharts.js
@@ -1,5 +1,3 @@
-//import { polygonHull } from "d3";
-
 /*
   Version: 1.0
   Created date: 29 Nov 2019
@@ -294,32 +292,11 @@ $(document).ready(function (){
     }
 });
 
-//Creat Map Chart
-// $(document).ready(function () {
-//     d3.json("map/LAO_ADM1.json").then(map);
-//     function map(lao) {
-//     let province = ChartGeo.topojson.(lao, lao.objects.LAO_ADM1).features;
-//     let vte = province.find((d) => d.properties.Name === 'Vientiane Capital');
-//     let ph = province.find((d) => d.properties.Name === 'Phongsaly');
-//     console.log(province);
-//     let Map = new Chart(document.getElementById("Map").getContext("2d"), {
-//     type: 'choropleth',
-//     data: {
-//       labels: province.map(d => d.properties.Name),
-//       datasets: [{
-//         label: 'Province',
-//         data: [{
-//             value: 0.1,
-//             feature: vte
-//         }, {
-//             value:0.9,
-//             feature: ph
-//         }],
-//       }]
-//     }
-//   });
-// };
-// });
+
+
+//Section Create Map
+
+//Stunting 2011 Map
 $(document).ready(function () {
     //Set variable map directory
     let mapDraw = ("map/LAO_ADM1.json");
@@ -328,8 +305,8 @@ $(document).ready(function () {
     let svg = d3.select("#test");
     //Set Scale
     let colorScale = d3.scaleThreshold()
-        .domain([2.4, 2.5, 10.0, 20.0, 30.0])
-        .range(d3.schemeBlues[6]);
+        .domain([0, 0.025, 0.10, 0.20, 0.30])
+        .range(["#fafafa", "#0091ea",  "#00c853",  "#ffd600", "#ff6d00", "#d50000"]);
     //Set tooltips
     let tooltip1 = d3.select("body").append("div") 
     .attr("class", "tooltip")       
@@ -359,7 +336,7 @@ $(document).ready(function () {
             .enter()
             .append("path")
             .attr("d", d3.geoPath().projection(projection))
-            .attr("fill", d => colorScale(d.properties.feature_id = stuntingSort.get(d.properties.feature_id)));
+            .attr("fill", d => colorScale((d.properties.feature_id = stuntingSort.get(d.properties.feature_id))/100));
     
         svg.selectAll("path")
             .data(myMap.features)
@@ -382,9 +359,23 @@ $(document).ready(function () {
             .datum(topojson.mesh(lao, lao.objects.LAO_ADM1, function(a, b) { return a !== b; }))
             .attr("class", "mapBorder")
             .attr("d", d3.geoPath().projection(projection));
+
+        //Add Legend
+        svg.append("g")
+        .attr("transform", "translate(0,250)")
+        .append(() => legend({
+            color: d3.scaleThreshold(["<2.5", "2.5", "10", "20", ">=30"],
+            ["#0091ea",  "#00c853",  "#ffd600", "#ff6d00", "#d50000"]),
+            title: "WHO Classification, 2017 (%)",
+            width: 190}));
+    
+    
     }
 });
-//Map 11
+
+
+
+//Stunting 2017 Map
 $(document).ready(function () {
     //Set variable map directory
     let mapDraw = ("map/LAO_ADM1.json");
@@ -393,8 +384,8 @@ $(document).ready(function () {
     let svg = d3.select("#test1");
     //Set Scale
     let colorScale = d3.scaleThreshold()
-        .domain([2.4, 2.5, 10.0, 20.0, 30.0])
-        .range(d3.schemeBlues[6]);
+        .domain([0, 0.025, 0.10, 0.20, 0.30])
+        .range(["#fafafa", "#0091ea",  "#00c853",  "#ffd600", "#ff6d00", "#d50000"]);
     //Set tooltips
     let tooltip = d3.select("body").append("div") 
     .attr("class", "tooltip1")       
@@ -424,7 +415,7 @@ $(document).ready(function () {
             .enter()
             .append("path")
             .attr("d", d3.geoPath().projection(projection))
-            .attr("fill", d => colorScale(d.properties.feature_id = stuntingSort1.get(d.properties.feature_id)));
+            .attr("fill", d => colorScale((d.properties.feature_id = stuntingSort1.get(d.properties.feature_id))/100));
     
         svg.selectAll("path")
             .data(myMap.features)
@@ -447,5 +438,127 @@ $(document).ready(function () {
             .datum(topojson.mesh(lao, lao.objects.LAO_ADM1, function(a, b) { return a !== b; }))
             .attr("class", "mapBorder")
             .attr("d", d3.geoPath().projection(projection));
+
+        //Add legend
+        svg.append("g")
+        .attr("transform", "translate(0,250)")
+        .append(() => legend({
+            color: d3.scaleThreshold(["<2.5", "2.5", "10", "20", ">=30"],
+            ["#0091ea",  "#00c853",  "#ffd600", "#ff6d00", "#d50000"]),
+            title: "WHO Classification, 2017 (%)",
+            width: 190}));    
+    
+    
+    
     }
 });
+
+//Function to add legend to D3JS Map
+function ramp(color, n = 256) {
+    const canvas = DOM.canvas(n, 1);
+    const context = canvas.getContext("2d");
+    for (let i = 0; i < n; ++i) {
+      context.fillStyle = color(i / (n - 1));
+      context.fillRect(i, 0, 1, 1);
+    }
+    return canvas;
+  }
+
+  function legend({
+    color,
+    title,
+    tickSize = 6,
+    width = 320, 
+    height = 44 + tickSize,
+    marginTop = 18,
+    marginRight = 0,
+    marginBottom = 16 + tickSize,
+    marginLeft = 0,
+    ticks = width / 64,
+    tickFormat,
+    tickValues
+  } = {}) {
+  
+    const svg = d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .style("overflow", "visible")
+        .style("display", "block");
+  
+    let x;
+  
+    // Continuous
+    if (color.interpolator) {
+      x = Object.assign(color.copy()
+          .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+          {range() { return [marginLeft, width - marginRight]; }});
+  
+      svg.append("image")
+          .attr("x", marginLeft)
+          .attr("y", marginTop)
+          .attr("width", width - marginLeft - marginRight)
+          .attr("height", height - marginTop - marginBottom)
+          .attr("preserveAspectRatio", "none")
+          .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+  
+      // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+      if (!x.ticks) {
+        if (tickValues === undefined) {
+          const n = Math.round(ticks + 1);
+          tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+        }
+        if (typeof tickFormat !== "function") {
+          tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+        }
+      }
+    }
+  
+    // Discrete
+    else if (color.invertExtent) {
+      const thresholds
+          = color.thresholds ? color.thresholds() // scaleQuantize
+          : color.quantiles ? color.quantiles() // scaleQuantile
+          : color.domain(); // scaleThreshold
+  
+      const thresholdFormat
+          = tickFormat === undefined ? d => d
+          : typeof tickFormat === "string" ? d3.format(tickFormat)
+          : tickFormat;
+  
+      x = d3.scaleLinear()
+          .domain([-1, color.range().length - 1])
+          .rangeRound([marginLeft, width - marginRight]);
+  
+      svg.append("g")
+        .selectAll("rect")
+        .data(color.range())
+        .join("rect")
+          .attr("x", (d, i) => x(i - 1))
+          .attr("y", marginTop)
+          .attr("width", (d, i) => x(i) - x(i - 1))
+          .attr("height", height - marginTop - marginBottom)
+          .attr("fill", d => d);
+  
+      tickValues = d3.range(thresholds.length);
+      tickFormat = i => thresholdFormat(thresholds[i], i);
+    }
+  
+    svg.append("g")
+        .attr("transform", `translate(0, ${height - marginBottom})`)
+        .call(d3.axisBottom(x)
+          .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+          .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+          .tickSize(tickSize)
+          .tickValues(tickValues))
+        .call(g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+          .attr("y", marginTop + marginBottom - height - 6)
+          .attr("fill", "currentColor")
+          .attr("text-anchor", "start")
+          .attr("font-weight", "bold")
+          .text(title));
+  
+    return svg.node();
+  }
