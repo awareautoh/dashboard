8909983ff0e11387098546e19fa2e0d3d353c408
Douangtavanh <dk43454@gmail.com>
2020-02-17 17:32:14 +0700

    add page loading while waiting

---
+++ js/load.js
@@ -0,0 +1,14 @@
+let myVar;
+
+function load() {
+    myVar = setTimeout(showPage, 2000);
+}
+
+function showPage() {
+    document.getElementById("loadingImg").style.visibility = "hidden";
+    document.getElementById("content-header").style.visibility = "visible";
+    document.getElementById("content-container").style.visibility = "visible";
+    document.getElementById("content-footer").style.visibility = "visible";
+}
+load();
+showPage();
